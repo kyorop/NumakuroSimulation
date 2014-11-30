074a782493e859e3b38bcea6d03f9ae5b48905e8
coil398 <coil_the-world_1341398@i.softbank.jp>
2014-11-30 17:29:18 +0900

    オープニングから開始

--- NumakuroSimulation/NumakuroSimulation/OpeningScene.cpp
+++ NumakuroSimulation/NumakuroSimulation/OpeningScene.cpp
@@ -4,9 +4,11 @@
 
 OpeningScene::OpeningScene()
 {
+	x = 0;
 	op[0] = "今日、あなたのもとに新しいポケモンがやってきました。";
 	op[1] = "ミズゴロウを可愛がって\n立派なポケモンにしてあげてくださいね";
 	GHandle = LoadGraph("numakuro/OP.png");
+	input = false;
 }
 
 OpeningScene::~OpeningScene()
@@ -17,13 +19,21 @@ void OpeningScene::Update()
 {
 	if (CheckHitKey(KEY_INPUT_1))
 		ChangeScene(std::make_shared<IventSelectScene>());
-	if (CheckHitKey(KEY_INPUT_SPACE))x++;
+	if (CheckHitKey(KEY_INPUT_SPACE)&&!input)
+	{
+		x++;
+		input = true;
+	}
+	else if (!CheckHitKeyAll())
+	{
+		input = false;
+	}
 	if (x == 2)ChangeScene(std::make_shared<IventSelectScene>());
 }
 
 void OpeningScene::Draw()
 {
 	DrawGraph(0, 0, GHandle, true);
-	DrawString(50, 450, op[x], GetColor(0, 0, 0));
+	DrawFormatString(80, 80, GetColor(0, 0, 0), op[x]);
 	
 }
