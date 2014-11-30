85b491ba336c9efab88826221dbb495985955339
coil398 <coil_the-world_1341398@i.softbank.jp>
2014-11-30 17:50:37 +0900

    あああ

--- NumakuroSimulation/NumakuroSimulation/IventSelectScene.cpp
+++ NumakuroSimulation/NumakuroSimulation/IventSelectScene.cpp
@@ -13,6 +13,8 @@ IventSelectScene::IventSelectScene()
 	y(450)
 {
 	GHandle = LoadGraph("numakuro/ゲーム画面.png");
+	GMizu1 = LoadGraph("numakuro/1201508991_o.png");
+	GMizu2 = LoadGraph("numakuro/a23e082a.png");
 	ivents[0] = LoadGraph("numakuro/トレーニングアイコン.png");
 	ivents[1] = LoadGraph("numakuro/バトルアイコン.png");
 	ivents[2] = LoadGraph("numakuro/餌付けアイコン.png");
@@ -107,6 +109,8 @@ void IventSelectScene::Draw()
 		DrawGraph(x+60*i, y, ivents[i], true);
 	}
 
+
+
 	DrawBox(x_box, y_box, x_box + 50, y_box + 50, GetColor(255, 0, 0), false);
 	DrawBox(x_box+1, y_box+1, x_box + 49, y_box + 49, GetColor(255, 0, 0), false);
 
@@ -133,6 +137,13 @@ void IventSelectScene::Draw()
 	default: 
 		break;
 	}
+
+
+	if (IScene::gameManager->GetSatiety() >= 80)
+		GMizu = GMizu2;
+	else
+		GMizu = GMizu1;
+	DrawGraph(0, 0, GMizu, true);
 }
 
 
