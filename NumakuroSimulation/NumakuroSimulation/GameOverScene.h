e74f621ee356b56447a6fdd1ee02b0ede5ac1bb5
coil398 <coil_the-world_1341398@i.softbank.jp>
2014-11-30 16:47:44 +0900

    ゲームおーば―作った

--- NumakuroSimulation/NumakuroSimulation/GameOverScene.h
+++ NumakuroSimulation/NumakuroSimulation/GameOverScene.h
@@ -1,12 +1,16 @@
 ﻿#pragma once
 #include "iscene.h"
+#include <DxLib.h>
 
 class GameOverScene
 	:public IScene
 {
+private:
+	int GHandle = LoadGraph("numakuro/ゲームオーバー.png");
+	char *gameover[3];
 public:
 	void Update() override;
 	void Draw() override;
 public:
-	
+	GameOverScene();
 };
