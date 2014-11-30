94ddeb4826bd13f82f3b1e7016cabd1a3f93a5ee
moajo <mimirosiasd@gmail.com>
2014-11-30 13:57:54 +0900

    追加忘れ

--- NumakuroSimulation/NumakuroSimulation/Item.cpp
+++ NumakuroSimulation/NumakuroSimulation/Item.cpp
@@ -14,23 +14,35 @@ Item::~Item()
 //値段取得
 int Item::GetCost()
 {
-	return 0;
+	return 0;//仮実装
 }
 
 //アイテムの種類取得
 ItemType Item::GetType()
 {
-	return Food;
+	return Food;//仮実装
 }
 
 //アイテム名取得
 std::string Item::GetItemName()
 {
-	return "";
+	return "";//仮実装
 }
 
 //イメージハンドラ取得
 int Item::GetImageHandlar()
+{
+	return 0;//仮実装
+}
+
+
+
+int Food::GetRecoveryValue()
+{
+	return 0;//仮実装
+}
+
+int TrainingItem::GetRisingValue()
 {
 	return 0;
 }
