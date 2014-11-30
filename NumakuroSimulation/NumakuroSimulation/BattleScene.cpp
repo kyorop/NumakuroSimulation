80acb866f6431359091cc7a9d1f70144763e88ee
Kyoro_p <longcorridor@outlook.jp>
2014-11-30 17:24:54 +0900

    init呼ばれるようにした

--- NumakuroSimulation/NumakuroSimulation/BattleScene.cpp
+++ NumakuroSimulation/NumakuroSimulation/BattleScene.cpp
@@ -2,12 +2,38 @@
 #include <dxlib.h>
 #include "endingscene.h"
 
+
+BattleScene::BattleScene()
+{
+	isWon = false;
+	input = false;
+}
+
 void BattleScene::Update()
 {
 	if (CheckHitKey(KEY_INPUT_7))
 		ChangeScene(std::make_shared<EndingScene>());
+
+	if (CheckHitKey(KEY_INPUT_Z) && !input)
+	{
+		isWon = gameManager->DoBattle(nullptr);
+		input = true;
+	}
 }
 
 void BattleScene::Draw()
 {
+	if (!input)
+	{
+		DrawString(0, 0, "ｚを押すと戦います", GetColor(0, 0, 0));
+	}
+
+	if (isWon)
+	{
+		DrawString(0, 0, "ミズゴローは勝った", GetColor(0, 0, 0));
+	}
+	else
+	{
+		DrawString(0, 0, "ミズゴローは負けた", GetColor(0, 0, 0));
+	}
 }
